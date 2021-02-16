59a4714b7f35f53c01b8cd3c014e7e70b419ac88
Shadman Ahmed <shadmanahmed874@gmail.com>
2021-02-16 17:56:23 +0600

    Initial Commit

---
+++ classes/DbConnect.class.php
@@ -0,0 +1,15 @@
+<?php 
+
+class DbConnect {
+  private $host = "localhost";
+  private $user = "root";
+  private $pwd = "bT4sM2h8SuBV&@2a";
+  private $dbName = "oopblog";
+
+  public function connect() {
+    $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+    $pdo = new PDO($dsn, $this->user, $this->pwd);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+  }
+}
---
+++ templates/header.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" 
+        rel="stylesheet" 
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" 
+        crossorigin="anonymous">
+    <title>CRUD - php-oop-pdo</title>
+  </head>
+  <body>
+    <div class="container">
